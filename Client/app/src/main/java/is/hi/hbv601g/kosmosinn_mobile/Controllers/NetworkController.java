0bd96cfde3d5b0464da971128c73b1e500e3d914
Oskar Hoskuldsson <osh16@hi.is>
2021-03-17 20:25:17 +0000

    reyna laga til clutter

--- Client/app/src/main/java/is/hi/hbv601g/kosmosinn_mobile/Controllers/NetworkController.java
+++ Client/app/src/main/java/is/hi/hbv601g/kosmosinn_mobile/Controllers/NetworkController.java
@@ -1,4 +1,13 @@
 package is.hi.hbv601g.kosmosinn_mobile.Controllers;
 
+import java.util.List;
+
+import is.hi.hbv601g.kosmosinn_mobile.Entities.Board;
+
 public class NetworkController {
+    public List<Board> getAllBoards() {
+        String url = "http://10.5.0.2:8080/api/boards";
+
+    }
+
 }
