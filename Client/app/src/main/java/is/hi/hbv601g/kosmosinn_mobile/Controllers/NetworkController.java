942448ae73a3b160cbf60f4dde1edcff2cdcfb9d
Oskar Hoskuldsson <osh16@hi.is>
2021-03-23 20:39:36 +0000

    post, patch og delete virkni fyrir userrestcontroller

--- Client/app/src/main/java/is/hi/hbv601g/kosmosinn_mobile/Controllers/NetworkController.java
+++ Client/app/src/main/java/is/hi/hbv601g/kosmosinn_mobile/Controllers/NetworkController.java
@@ -5,9 +5,5 @@ import java.util.List;
 import is.hi.hbv601g.kosmosinn_mobile.Entities.Board;
 
 public class NetworkController {
-    public List<Board> getAllBoards() {
-        String url = "http://10.5.0.2:8080/api/boards";
-
-    }
 
 }
