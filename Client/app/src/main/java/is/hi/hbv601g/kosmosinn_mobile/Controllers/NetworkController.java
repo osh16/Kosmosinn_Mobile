4a05fa54e716341ca7b03c6607fb4f077361252b
Oskar Hoskuldsson <osh16@hi.is>
2021-03-28 19:52:47 +0000

    skastrik til ad laga allt

--- Client/app/src/main/java/is/hi/hbv601g/kosmosinn_mobile/Controllers/NetworkController.java
+++ Client/app/src/main/java/is/hi/hbv601g/kosmosinn_mobile/Controllers/NetworkController.java
@@ -38,8 +38,7 @@ import is.hi.hbv601g.kosmosinn_mobile.R;
 
 public class NetworkController {
 
-    //private static final String BASE_URL = "http://10.0.2.2:8080";
-    private static final String BASE_URL = "http://192.168.1.10:8080";
+    private static final String BASE_URL = "http://10.0.2.2:8080";
     private static NetworkController mInstance;
     private static RequestQueue mQueue;
     private Context mContext;
